83968c045b7c53f8e586f4d8b96de67844488a68
JensNordal <jensnordal@gmail.com>
2016-04-02 21:29:15 -0700

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+console.log('\'Hello \'Hello!'); // eslint-disable-line no-console
+
+$(".splendidBtn").click(function(){
+        alert("Woohoo! Yeoman scaffolding successfully completed.");
+    });
+
+//browser window scroll (in pixels) after which the "back to top" link is shown
+var offset = 0,
+	//duration of the top scrolling animation (in ms)
+	scroll_top_duration = 700;
+});
+
