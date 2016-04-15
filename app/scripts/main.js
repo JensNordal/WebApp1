ad50f24fd6e400883b5c2c36ef8a6692bc11cbb5
JensNordal <jensnordal@gmail.com>
2016-04-14 22:39:56 -0700

    Fixed theme switch error

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,24 +8,24 @@ $(".splendidBtn").click(function(){
 
 // Scroll to Top button
 var offset = 300,
-		//duration of the top scrolling animation (in ms)
-		scroll_top_duration = 700,
-		//grab the "back to top" link
-		$back_to_top = $('.cd-top');
+        //duration of the top scrolling animation (in ms)
+        scroll_top_duration = 700,
+        //grab the "back to top" link
+        $back_to_top = $('.cd-top');
 
-	//smooth scroll to top
-	$back_to_top.on('click', function(event){
-		event.preventDefault();
-		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
-	});
+        //smooth scroll to top
+        $back_to_top.on('click', function(event){
+            event.preventDefault();
+            $('body,html').animate({
+                scrollTop: 0,
+            }, scroll_top_duration
+            );
+        });
 
 
 // Alternate Stylesheet Script
 function swapStyleSheet(sheet) {
-    document.getElementById("pageStyle").setAttribute("href", sheet);  
+    document.getElementById("pageStyle").setAttribute("href",sheet);
 }
 
 function initate() {
@@ -33,7 +33,7 @@ function initate() {
     var style2 = document.getElementById("stylesheet2");
 
     style1.onclick = function () { swapStyleSheet("styles/main.css") };
-	style2.onclick = function () { swapStyleSheet("styles/alternate.css"); };
+    style2.onclick = function () { swapStyleSheet("styles/alternate.css"); };
 }
 
 window.onload = initate;
